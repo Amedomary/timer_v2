8f26665a1941b072554dd3da926edfac44f89e85
Amed <amedomaryyo93@mail.ru>
2019-10-14 22:09:39 +0400

    add pug

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,14 @@
-<template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+<template lang="pug">
+    div(id="app")
+        div(id="nav")
+          router-link(to="/") Home |
+          router-link(to="/about") About
+        router-view
 </template>
 
 <style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
